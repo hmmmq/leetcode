22bec4c8d50f68f46aa99b026800856519328f32
hmmmq <1053825066@qq.com>
2023-12-03 02:16:09 +0000

    add detailed explaination

--- src/lee22/Solution.java
+++ src/lee22/Solution.java
@@ -3,6 +3,27 @@ package lee22;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 22. 括号生成
+ * leecode: https://leetcode.com/problems/generate-parentheses/description/
+ * 题解：https://leetcode.com/problems/generate-parentheses/solutions/4355549/java-detail-solution-chinese/
+ * 题目描述：
+ *    Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ *    For example, given n = 3, a solution set is:
+ *    [ "((()))", "(()())", "(())()", "()(())", "()()()" ]
+ *    Given n = 2, a solution set is:
+ *    [ "(())", "()()" ]
+ *    Given n = 1, a solution set is:
+ *    [ "()" ]
+ *    Given n = 0, a solution set is:
+ *    [ ]
+ * 思路：
+ *   1. 递归
+ * 时间复杂度：
+ *  1. O(2^n)
+ * 空间复杂度：
+ *  1. O(n*2^n)
+ */
 public class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
