a78112042ad62e8bc111fd0dd971ca2f8e029259
hmmmq <1053825066@qq.com>
2023-12-04 01:59:16 +0000

    add solution for leecode 23

---
+++ src/lee23/ListNode.java
@@ -0,0 +1,88 @@
+package lee23;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    private String listToString(ListNode list) {
+        StringBuilder sb = new StringBuilder();
+        while (list != null) {
+            sb.append(list.val).append(" -> ");
+            list = list.next;
+        }
+        sb.append("null");
+        return sb.toString();
+    }
+
+    // Helper function to merge two sorted lists and print the merging process
+    private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+
+        return dummyHead.next;
+    }
+
+    // Main function to merge k sorted lists
+    public ListNode mergeKLists(ListNode[] lists) {
+        int n = lists.length;
+        if (n == 0) {
+            return null;
+        } else if (n == 1) {
+            return lists[0];
+        }
+
+        // Merge lists in pairs until there is only one list left
+        int interval = 1;
+        while (interval < n) {
+            System.out.println();
+            for (int i = 0; i < n - interval; i += interval * 2) {
+                String indent = "  ".repeat(interval);
+                System.out.println(indent + "Interval: " + interval);
+                System.out.println(indent + "Merge List: " + i + " and List: " + (i + interval));
+                System.out.println(indent + "List "+ i + ": " + listToString(lists[i]));
+                System.out.println(indent + "List "+ (i + interval) + ": " + listToString(lists[i + interval]));
+                lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+                System.out.println(indent + "Merged list: " + listToString(lists[i]));
+                System.out.println(indent + "Interval: " + interval);
+                System.out.println();
+            }
+            interval *= 2;
+        }
+
+        return lists[0];
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode[] lists = new ListNode[5];
+        lists[0] = new ListNode(1, new ListNode(4, new ListNode(5)));
+        lists[1] = new ListNode(1, new ListNode(3, new ListNode(4)));
+        lists[2] = new ListNode(2, new ListNode(6));
+        lists[3] = new ListNode(3, new ListNode(7));
+        lists[4] = new ListNode(4, new ListNode(8));
+        solution.mergeKLists(lists);
+    }
+}
