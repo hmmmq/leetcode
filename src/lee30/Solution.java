a5ab0d6b3ac8d80b1587f247a71c23a57e425290
hmmmq <1053825066@qq.com>
2023-12-08 18:22:44 +0000

    create lee30

---
+++ src/lee30/Solution.java
@@ -0,0 +1,52 @@
+package lee30;
+
+import java.util.*;
+
+/**
+ * 30. Substring with Concatenation of All Words
+ * description: https://leetcode.com/problems/substring-with-concatenation-of-all-words/description/
+ */
+class Solution {
+
+    public List<Integer> findSubstring(String s, String[] words) {
+        var m = words.length;
+        var n = words[0].length();
+        var result = new ArrayList<Integer>();
+
+        // check if the length of s is less than m * n
+        if (s.length() < m * n)
+            return result;
+
+        // build the hashmap
+        var map = new HashMap<String, Integer>();
+        for (var word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        // traverse all the possible start index
+        for (var i = 0; i <= s.length() - m * n; i++) {
+            // traverse the window with size n
+            // build a hashmap for the window
+            var windowMap = new HashMap<String, Integer>();
+            // count represents the number of satisfied words
+            for (var j = 0; j < m; j++) {
+                // extract a word from the window
+                var temp = s.substring(i + j * n, i + j * n + n);
+                // add the word to the window hashmap
+                windowMap.put(temp, windowMap.getOrDefault(temp, 0) + 1);
+            }
+            // check if the current window is what we want
+            if (windowMap.equals(map))
+                result.add(i);
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String[] words = {"foo", "bar"};
+        System.out.println(s.findSubstring("barfoothefoobarman", words));
+    }
+
+
+}
