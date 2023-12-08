b75236d46b4d45517369d8a4293abf03e81ed31f
hmmmq <1053825066@qq.com>
2023-12-08 15:26:26 +0000

    revise lee28

--- src/lee28/Solution.java
+++ src/lee28/Solution.java
@@ -10,27 +10,41 @@ package lee28;
  * 空间复杂度O(n)
  */
 class Solution {
-    public int[] buildNext(String needleStr){
+
+    /**
+     * 构建next数组步骤
+     * 1.初始化next[0]=0
+     * 2.初始化i=1,j=0
+     * 3.如果needle[i]==needle[j],则next[i]=j+1,i++,j++
+     * 4.如果needle[i]!=needle[j],则回溯j=next[j-1]
+     * 5.如果j==0,则next[i]=0,i++
+     * 6.重复3-5
+     * 7.返回next数组
+     * @param needleStr 子串
+     * @return next数组
+     */
+    public int[] buildNext(String needleStr) {
 
         int[] next = new int[needleStr.length()];
         int j = 0;
         int i = 1;
         char[] needle = needleStr.toCharArray();
 
-        while( i < needle.length){
+        while (i < needle.length) {
 
-            if(needle[i] == needle[j]){
+            if (needle[i] == needle[j]) {
 
                 next[i++] = ++j;
 
-            }else{
+            } else {
 
-                if(j==0){
+                if (j == 0) {
 
-                    next[i++]=0;
+                    next[i++] = 0;
 
-                }else{
-                    j = next[j-1];
+                } else {
+                    // 回溯
+                    j = next[j - 1];
                 }
 
             }
@@ -38,6 +52,19 @@ class Solution {
         return next;
 
     }
+
+    /**
+     * KMP算法
+     * 1.初始化i=0,j=0
+     * 2.如果haystack[i]==needle[j],则i++,j++
+     * 3.如果haystack[i]!=needle[j],则回溯j=next[j-1]
+     * 4.如果j==0,则i++
+     * 5.重复2-4
+     * 6.返回结果
+     * @param haystackStr 母串
+     * @param needleStr 子串
+     * @return 子串在母串中的位置
+     */
     public int strStr(String haystackStr, String needleStr) {
 
         int i = 0;
@@ -48,38 +75,36 @@ class Solution {
         char[] haystack = haystackStr.toCharArray();
         char[] needle = needleStr.toCharArray();
 
-        while( i < m ){
+        while (i < m) {
 
-            if(haystack[i] == needle[j]){
+            if (haystack[i] == needle[j]) {
 
                 i++;
                 j++;
 
-            }else{
+            } else {
 
-                if(j>0){
+                if (j > 0) {
+                    // 回溯
+                    j = next[j - 1];
 
-                    j = next[j-1];
-
-                }else{
+                } else {
                     i++;
                 }
             }
 
-            if(j==n){
-                return i-n;
+            if (j == n) {
+                return i - n;
             }
 
         }
 
-
         return -1;
-
 
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.strStr("hello","ll"));
+        System.out.println(s.strStr("hello", "ll"));
     }
 }
